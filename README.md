45c680b4042343152e473f619651b7834babc2a7
chester pressler <chester.pressler@softvision.com>
2020-05-04 11:58:46 -0700

    updated project ot use nexus emulator

--- README.md
+++ README.md
@@ -79,15 +79,20 @@ appium-doctor
 ## Running the tests
 
 1. make sure appium server is running by running from Mac Searchlight
-2. Verify Appium laucnes and select "Start Server X.xx.x"
+2. Verify Appium launches and select "Start Server X.xx.x"
 3. Change to project root directory ( where pom.xml is)
 3. mvn clean test -Dplatform=ios   
 4. mvn clean test -Dplatform=android
+5. mvn test -Dplatform=android -Dtestsite=local
+6. mvn test -Dplatform=android -Dtestsite=saucelabs
+7. mvn test -Dplatform=ios -Dtestsite=local
+8. mvn test -Dplatform=ios -Dtestsite=saucelabs
 
 FIX_ME   
 Need to have the following running before running tests
 1. Appium Server started
-2. Android emulator started in background
+2. Android emulator started in background  
+    ~/Libary/Android/sdk/tool directory ->  emulator -avd Pixal_3a_API_29
 3. Ios Simulator started in background
 
 #### Example python tests
@@ -105,3 +110,47 @@ npm install -g appium  # get appium
 npm install wd         # get appium client   
 appium &               # start appium   
 node your-appium-test.js
+
+#### Issue
+Connection refused (Connection refused)
+Build info: version: '3.141.59', revision: 'e82be7d358', time: '2018-11-14T08:17:03'
+System info: host: 'svuslp00099-170.local', ip: 'fe80:0:0:0:822:ab64:2571:71c5%en0', os.name: 'Mac OS X', os.arch: 'x86_64', os.version: '10.15.4', java.version: '1.8.0_212'
+Driver info: driver.version: IOSDriver
+        at com.softvision.simplemobile.LoginTest.getDriver(LoginTest.java:136)
+        at com.softvision.simplemobile.LoginTest.initAndroid(LoginTest.java:116)
+        at com.softvision.simplemobile.LoginTest.init(LoginTest.java:71)
+Caused by: java.net.ConnectException: Connection refused (Connection refused)
+        at com.softvision.simplemobile.LoginTest.getDriver(LoginTest.java:136)
+        at com.softvision.simplemobile.LoginTest.initAndroid(LoginTest.java:116)
+        at com.softvision.simplemobile.LoginTest.init(LoginTest.java:71)
+
+[ERROR] com.softvision.simplemobile.LoginTest  Time elapsed: 0.53 s  <<< ERROR!
+java.lang.NullPointerException
+        at com.softvision.simplemobile.LoginTest.afterTest(LoginTest.java:121)
+
+[INFO] 
+[INFO] Results:
+[INFO] 
+[ERROR] Errors: 
+[ERROR] com.softvision.simplemobile.LoginTest.null
+[ERROR]   Run 1: LoginTest.init:71->initAndroid:116->getDriver:136 » WebDriver Connection refus...
+[ERROR]   Run 2: LoginTest.afterTest:121 NullPointer
+[INFO] 
+[INFO] 
+[ERROR] Tests run: 1, Failures: 0, Errors: 1, Skipped: 0
+
+### Fix is 
+Start Appium Server
+
+####Issue
+[INFO] 
+[INFO] Results:
+[INFO] 
+[ERROR] Errors: 
+[ERROR] com.softvision.simplemobile.LoginTest.null
+[ERROR]   Run 1: LoginTest.init:71->initAndroid:116->getDriver:136 » SessionNotCreated Unable t...
+[ERROR]   Run 2: LoginTest.afterTest:121 NullPointer
+[INFO] 
+
+####Fix
+Start the emulator
--- src/test/java/com/softvision/simplemobile/LoginTest.java
+++ src/test/java/com/softvision/simplemobile/LoginTest.java
@@ -38,11 +38,18 @@ public class LoginTest {
     static String myApp = "myApp";
     //static String androidApp = "/Users/chesterpressler/development/appium/loginSimpleExample/android/LoginDemo/app/build/outputs/apk/debug/app-debug.apk";
     //static String iosApp = "/Users/chesterpressler/Downloads/appium-ios-basic-master/final/build/Release-iphonesimulator/AppiumTest.app";
-    static String androidApp = "src/test/resources/android/app-debug.apk";
-    static String iosApp = "src/test/resources/ios/AppiumTest.app";
+    static String androidApp = "app-debug.apk";
+    static String iosApp = "AppiumTest.app";
+    static String androidAppLocation = "src/test/resources/android/";
+    static String iosAppLocation = "src/test/resources/ios/";
 
     static MobileDriver mobileDriver;
-    private static String APPIUM_URL = "http://localhost:4723/wd/hub/";
+    private static String testLocation = "local";
+    private static String appiumUrl = "http://localhost:4723/wd/hub/";
+    private static String seleniumURI = "@ondemand.saucelabs.com:443";
+    private static String buildTag = System.getenv("BUILD_TAG");
+    private static String username = System.getenv("SAUCE_USERNAME");
+    private static String accesskey = System.getenv("SAUCE_ACCESS_KEY");
     private static String mobilePlatform = "android";
     private static String attributeTextValue = "text";
     private static String attributeEnabledValue = "enabled";
@@ -50,11 +57,15 @@ public class LoginTest {
     @BeforeClass
     public static void init() throws MalformedURLException {
         String platform = System.getProperty("platform");
+        String testsite = System.getProperty("testsite");
         System.out.println("platform property is " + platform);
-        System.out.println("appium.java.client.version property is " + System.getProperty("appium.java.client.version"));
+        System.out.println("testsite property is " + testsite);
 
         if ( platform != null ) {
             mobilePlatform = platform;
+        }
+        if ( testsite != null ) {
+            testLocation = testsite;
         }
         if ( "android".equalsIgnoreCase(mobilePlatform)){
             initAndroid();
@@ -69,28 +80,40 @@ public class LoginTest {
         System.out.println("initIOS() method called");
         attributeTextValue = "value";
         attributeEnabledValue = "wdVisible";
-        File app = new File(iosApp);
+
         capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.4");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
-        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        capabilities.setCapability(MobileCapabilityType.APP, getApp() );
 
-        mobileDriver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        //mobileDriver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        mobileDriver = getDriver();
     }
 
     public static void initAndroid() throws MalformedURLException {
         System.out.println("initAndroid() method called");
         attributeTextValue = "text";
         attributeEnabledValue = "enabled";
-        File app = new File(androidApp);
+
         capabilities = new DesiredCapabilities();
+
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        capabilities.setCapability(MobileCapabilityType.APP, getApp());
+        capabilities.setCapability("browserName", "");
+        capabilities.setCapability("appiumVersion", "1.17.0");
 
-        mobileDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+//        capabilities.setCapability("appiumVersion", "1.16.0");
+//        capabilities.setCapability("deviceName","Samsung Galaxy S9 WQHD GoogleAPI Emulator");
+//        capabilities.setCapability("deviceOrientation", "portrait");
+//        capabilities.setCapability("browserName", "");
+//        capabilities.setCapability("platformVersion","9.0");
+//        capabilities.setCapability("platformName","Android");
+//        capabilities.setCapability("app","sauce-storage:app-debug.apk");
+
+        mobileDriver = getDriver();
     }
 
     @AfterClass
@@ -98,12 +121,62 @@ public class LoginTest {
         mobileDriver.quit();
     }
 
+    public static MobileDriver getDriver ()  {
+        if ( "local".equalsIgnoreCase(testLocation)) {
+            appiumUrl = "http://127.0.0.1:4723/wd/hub";
+            System.out.println("local appiumUrl =" + appiumUrl);
+        } else if ( "saucelabs".equalsIgnoreCase("saucelabs")) {
+            appiumUrl = "https://" + username + ":" + accesskey + seleniumURI +"/wd/hub";
+            System.out.println("remote appiumUrl =" + appiumUrl);
+        } else {
+            appiumUrl = "http://127.0.0.1:4723/wd/hub";
+        }
+        try {
+        if ( "android".equalsIgnoreCase(mobilePlatform)){
+            return  new IOSDriver(new URL(appiumUrl), capabilities);
+        } else if ( "ios".equalsIgnoreCase(mobilePlatform)){
+            return new IOSDriver(new URL(appiumUrl), capabilities);
+        } else {
+            return null;
+        }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public static String getApp ()  {
+        String appName = "";
+        if ( "local".equalsIgnoreCase(testLocation)) {
+            if ( "android".equalsIgnoreCase(mobilePlatform)){
+                File app = new File(androidAppLocation + androidApp);
+                appName = app.getAbsolutePath();
+            } else if ( "ios".equalsIgnoreCase(mobilePlatform)){
+                File app = new File(iosAppLocation + iosApp);
+                appName = app.getAbsolutePath();
+            }
+            System.out.println("local appName =" + appName);
+        } else if ( "saucelabs".equalsIgnoreCase("saucelabs")) {
+            if ( "android".equalsIgnoreCase(mobilePlatform)){
+                //appName = "sauce-storage:" + androidApp;
+                appName = "https://github.com/cpressler/simpleMobileTest/blob/master/src/test/resources/android/app-debug.apk";
+            } else if ( "ios".equalsIgnoreCase(mobilePlatform)){
+                //appName = iosApp;
+                appName = "sauce-storage:" + "myApp.zip";
+            }
+            System.out.println("saucelabs appName =" + appName);
+        }
+        return appName;
+
+    }
+
     @Test
     public void emailFieldInput() throws InterruptedException {
 
         MobileElement emailTF = (MobileElement) mobileDriver.findElement(MobileBy.AccessibilityId("emailTextField"));
 
-        emailTF.sendKeys("");
+        //emailTF.sendKeys("");
         emailTF.sendKeys("validEmail");
         //emailTF.sendKeys(Keys.RETURN);
         sleep(1000);
@@ -139,5 +212,4 @@ public class LoginTest {
         assertTrue(Boolean.parseBoolean(smiley.getAttribute(attributeEnabledValue)));
         mobileDriver.resetApp();
     }
-
 }
